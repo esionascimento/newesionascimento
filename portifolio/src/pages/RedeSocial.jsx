723dca5bb9178932b9cd5d5727466b12bff22ba1
esionascimento <esio.392@protonmail.com>
2021-06-20 14:54:13 -0400

    add route, add page About, RedeSocial

---
+++ portifolio/src/pages/RedeSocial.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class RedeSocial extends Component {
+  render() {
+    return (
+      <div>
+        Rede Social
+      </div>
+    )
+  }
+}
